953f7de2424035d0568e785ee6ea5d5cfbb38f3a
PTT31 <tung3108vn@gmail.com>
2023-11-14 23:29:05 +0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,57 @@
-#include "sd_.h"            // Thư viện để làm việc với thẻ SD
+#include "sd_.h" // Thư viện để làm việc với thẻ SD
 #include "lcd.h"
-#include "RTClib.h"         // Thư viện để làm việc với module RTC DS1307
-#include <Adafruit_Fingerprint.h>  // Thư viện để làm việc với cảm biến vân tay
-#include <WiFi.h>           // Thư viện WiFi
-#include <NTPClient.h>      // Thư viện NTPClient cho đồng bộ thời gian
-#include <DNSServer.h>      // Thư viện DNSServer để xử lý DNS trên AP
-#include <AsyncTCP.h>       // Thư viện AsyncTCP cho ESPAsyncWebServer
-#include "ESPAsyncWebServer.h"  // Thư viện ESPAsyncWebServer
-#define MAX_LINE_LENGTH (64)  // Định nghĩa độ dài tối đa của một dòng trong file
-#define mySerial Serial2     // Sử dụng Serial2 cho cảm biến vân tay
+#include "RTClib.h"               // Thư viện để làm việc với module RTC DS1307
+#include <Adafruit_Fingerprint.h> // Thư viện để làm việc với cảm biến vân tay
+#include <WiFi.h>                 // Thư viện WiFi
+#include <NTPClient.h>            // Thư viện NTPClient cho đồng bộ thời gian
+#include <DNSServer.h>            // Thư viện DNSServer để xử lý DNS trên AP
+#include <AsyncTCP.h>             // Thư viện AsyncTCP cho ESPAsyncWebServer
+#include "ESPAsyncWebServer.h"    // Thư viện ESPAsyncWebServer
+#define MAX_LINE_LENGTH (64)      // Định nghĩa độ dài tối đa của một dòng trong file
+#define mySerial Serial2          // Sử dụng Serial2 cho cảm biến vân tay
 
-RTC_DS1307 rtc;  // Khai báo đối tượng RTC
+RTC_DS1307 rtc; // Khai báo đối tượng RTC
 
-SemaphoreHandle_t spiMutex;  // Semaphore để quản lý truy cập SPI
+SemaphoreHandle_t spiMutex; // Semaphore để quản lý truy cập SPI
 
-QueueHandle_t QueueHandle;  // Hàng đợi để truyền ID từ TaskFinger đến TaskSQL
-const int QueueElementSize = 10;  // Số lượng phần tử tối đa trong hàng đợi
-TaskHandle_t taskfinger;  // Handle của TaskFinger
-TaskHandle_t tasklcd;     // Handle của TaskLCD
-TaskHandle_t tasksql;     // Handle của TaskSQL
-TaskHandle_t taskitn;     // Handle của TaskInternet
+QueueHandle_t QueueHandle;       // Hàng đợi để truyền ID từ TaskFinger đến TaskSQL
+const int QueueElementSize = 10; // Số lượng phần tử tối đa trong hàng đợi
+TaskHandle_t taskfinger;         // Handle của TaskFinger
+TaskHandle_t tasklcd;            // Handle của TaskLCD
+TaskHandle_t tasksql;            // Handle của TaskSQL
+TaskHandle_t taskitn;            // Handle của TaskInternet
 
-enum FingerMode {
+enum FingerMode
+{
     Scan_finger,
     Insert_finger,
     Correct_finger,
     Incorrect_finger
 };
 
-class message_lcd {
+class message_lcd
+{
 public:
     int mode = Scan_finger;
     char ip[20] = "Conect Wifi...";
-    char noti[20];
+    const char *noti;
 };
 
-
-User_if user;    // Đối tượng để chứa Thông tin ngươi quét vân tay
-message_lcd message;  // Đối tượng để chứa thông điệp cho TaskLCD
-void TaskFinger(void *pvParameters);  // Hàm của TaskFinger
-DateTime TaskTime();  // Hàm để lấy thời gian từ module DS1307
-void TaskLCD(void *pvParameters);  // Hàm của TaskLCD
-void TaskSQL(void *pvParameters);  // Hàm của TaskSQL
-void TaskInternet(void *pvParameters);  // Hàm của TaskInternet
-int Finger_s();  // Hàm để đọc ID từ cảm biến vân tay
-void Record();  // Hàm để ghi dữ liệu
+User_if user;                          // Đối tượng để chứa Thông tin ngươi quét vân tay
+message_lcd message;                   // Đối tượng để chứa thông điệp cho TaskLCD
+void TaskFinger(void *pvParameters);   // Hàm của TaskFinger
+DateTime TaskTime();                   // Hàm để lấy thời gian từ module DS1307
+void TaskLCD(void *pvParameters);      // Hàm của TaskLCD
+void TaskSQL(void *pvParameters);      // Hàm của TaskSQL
+void TaskInternet(void *pvParameters); // Hàm của TaskInternet
+int Finger_s();                        // Hàm để đọc ID từ cảm biến vân tay
+void Record();                         // Hàm để ghi dữ liệu
 // In Thoi Gian
-void drawTime(U8G2_ST7567_JLX12864_F_4W_HW_SPI u8g2);  // Hàm để vẽ thời gian lên màn hình
+void drawTime(U8G2_ST7567_JLX12864_F_4W_HW_SPI u8g2); // Hàm để vẽ thời gian lên màn hình
 // Ve File
-void drawFile(u8g2_int_t x, u8g2_int_t y, const char *filename, U8G2_ST7567_JLX12864_F_4W_HW_SPI u8g2);  // Hàm để vẽ hình ảnh từ file lên màn hìn
-void record(int id);  // Hàm để ghi dữ liệu vào cơ sở dữ liệu
-int Finger_s(Adafruit_Fingerprint finger);  // Hàm để đọc ID từ cảm biến vân tay
+void drawFile(u8g2_int_t x, u8g2_int_t y, const char *filename, U8G2_ST7567_JLX12864_F_4W_HW_SPI u8g2); // Hàm để vẽ hình ảnh từ file lên màn hìn
+void record(int id);                                                                                    // Hàm để ghi dữ liệu vào cơ sở dữ liệu
+int Finger_s(Adafruit_Fingerprint finger);                                                              // Hàm để đọc ID từ cảm biến vân tay
 
 // In Thoi Gian
 void drawTime(U8G2_ST7567_JLX12864_F_4W_HW_SPI u8g2)
@@ -69,7 +70,7 @@ void drawTime(U8G2_ST7567_JLX12864_F_4W_HW_SPI u8g2)
 };
 void setup()
 {
-    //Ham cấu hình
+    // Ham cấu hình
     Serial.begin(115200);
     while (!Serial)
     {
@@ -120,7 +121,7 @@ void record(int id)
 
 int Finger_s(Adafruit_Fingerprint finger)
 {
-    //test nên viết ntn nào dùng cảm biến vân tay thì thay code
+    // test nên viết ntn nào dùng cảm biến vân tay thì thay code
     if (Serial.available() > 0)
     {
         // Đọc số nguyên từ cổng Serial
@@ -178,16 +179,13 @@ void TaskLCD(void *pvParameters)
             switch (message.mode)
             {
             case Scan_finger:
-                u8g2.drawFile(0, 40, "/bin/u8g2.bin");
+                u8g2.drawFile(0, 40, "/bin/Scan_finger.bin");
                 break;
             case Correct_finger:
                 Serial.println(message.noti);
-                u8g2.setCursor(0, 40); // Đặt vị trí để in tên
+                u8g2.setCursor(0, 30);    // Đặt vị trí để in tên
                 u8g2.print(message.noti); // In tên lên màn hình
                 u8g2.drawFile(0, 40, "/bin/Correct_finger.bin");
-                vTaskDelay(pdMS_TO_TICKS(2000));
-                memset(message.noti, 0, 20);
-                message.mode = Scan_finger;
                 break;
             case Insert_finger:
                 u8g2.drawFile(0, 40, "/bin/Insert_finger.bin");
@@ -198,7 +196,7 @@ void TaskLCD(void *pvParameters)
             default:
                 break;
             }
-        current_u8g_page++;
+            current_u8g_page++;
         } while (u8g2.nextPage());
         current_u8g_page = 0;
         Serial.println(xPortGetFreeHeapSize());
@@ -218,14 +216,18 @@ void TaskSQL(void *pvParameters)
             Serial.printf("ID find %d:\"\n", finger_id);
             xSemaphoreTake(spiMutex, portMAX_DELAY);
             db_query(finger_id, &user);
-            if(user.name != NULL){
+            if (user.name != NULL)
+            {
 
-                strcpy(message.noti, user.name);
+                message.noti = user.name;
                 message.mode = Correct_finger;
-                Serial.printf("Name: %s, Finger_id: %d\n", user.name, user.finger_id);
+                user.name = NULL;
+                // Serial.printf("Name: %c, Finger_id: %d\n", user.name, user.finger_id);
             }
             xSemaphoreGive(spiMutex);
-            
+            vTaskDelay(5000);
+            message.noti = "";
+            message.mode = Scan_finger;
         }
         else if (ret == pdFALSE)
         {
@@ -235,8 +237,7 @@ void TaskSQL(void *pvParameters)
 }
 const char *ssid = "A";
 const char *password = "tung3108";
-AsyncWebServer server(80);
-DNSServer dnsServer;
+
 void notFound(AsyncWebServerRequest *request)
 {
     request->send(404, "text/plain", "Not found");
@@ -244,39 +245,41 @@ void notFound(AsyncWebServerRequest *request)
 
 void TaskInternet(void *pvParameters)
 {
-    WiFiUDP ntpUDP;
-    NTPClient timeClient(ntpUDP, "pool.ntp.org", 7 * 3600);
+    // AsyncWebServer server(80);
+    // DNSServer dnsServer;
+    // WiFiUDP ntpUDP;
+    // NTPClient timeClient(ntpUDP, "pool.ntp.org", 7 * 3600);
 
-    WiFi.begin(ssid, password);
-    Serial.print("Connecting to WiFi...");
-    for (int count; count < 20 && WiFi.status() != WL_CONNECTED; count++)
-    {
-        Serial.print(".");
-        vTaskDelay(pdMS_TO_TICKS(500));
-    }
-    if (WiFi.status() != WL_CONNECTED)
-    {
-        Serial.print("Wifi STA");
+    // WiFi.begin(ssid, password);
+    // Serial.print("Connecting to WiFi...");
+    // for (int count; count < 20 && WiFi.status() != WL_CONNECTED; count++)
+    // {
+    //     Serial.print(".");
+    //     vTaskDelay(pdMS_TO_TICKS(500));
+    // }
+    // if (WiFi.status() != WL_CONNECTED)
+    // {
+    //     Serial.print("Wifi STA");
 
-        WiFi.softAP("esp-captive");
-    }
-    else
-    {
-        Serial.println("");
-        Serial.print("Connected to ");
-        Serial.println(ssid);
-        Serial.print("IP address: ");
-        Serial.println(WiFi.localIP());
-        timeClient.begin();
-        timeClient.update();
-        rtc.adjust(DateTime(timeClient.getEpochTime()));
-        // server.on("/", handleRoot);
-        // server.on("/login", handleLogin);
-        // server.on("/wifi", handleWifi);
-        // server.on("/save", handleSave);
+    //     WiFi.softAP("esp-captive");
+    // }
+    // else
+    // {
+    //     Serial.println("");
+    //     Serial.print("Connected to ");
+    //     Serial.println(ssid);
+    //     Serial.print("IP address: ");
+    //     Serial.println(WiFi.localIP());
+    //     timeClient.begin();
+    //     timeClient.update();
+    //     rtc.adjust(DateTime(timeClient.getEpochTime()));
+    //     // server.on("/", handleRoot);
+    //     // server.on("/login", handleLogin);
+    //     // server.on("/wifi", handleWifi);
+    //     // server.on("/save", handleSave);
 
-        server.begin();
-    }
+    //     server.begin();
+    // }
     while (1)
     {
         vTaskDelay(pdMS_TO_TICKS(5000));
